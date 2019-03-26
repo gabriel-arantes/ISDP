73cdadc4eda114037ae634f75a1ff04f0196e1b9
gabriel-arantes <38770367+gabriel-arantes@users.noreply.github.com>
2019-03-26 15:56:29 -0300

    Update functions.py

--- main/functions.py
+++ main/functions.py
@@ -2,6 +2,10 @@ import csv
 import time
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 def openISDP(driver):
     driver.get('http://app-br.huawei.com/sdcp/portalNew#!isd/webix/rolloutPlanManagment.html')
@@ -9,6 +13,13 @@ def openISDP(driver):
     time.sleep(2)
 
 def site_login(filepath,driver):
+    try:
+        element = WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located((By.NAME, "Submit"))
+        )
+    except:
+        print("Problemas de conexão")
+
     with open(filepath, "r") as th:
         lines = csv.reader(th)
         for line in lines:
@@ -17,13 +28,26 @@ def site_login(filepath,driver):
             driver.find_element_by_id("uid").send_keys(username)
             driver.find_element_by_id("password").send_keys(password)
             driver.find_element_by_name("Submit").click()
-            time.sleep(3)
+
 
 def chooseProject(driver):
+    try:
+        element = WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located((By.XPATH, "//div[@class='project']"))
+        )
+    except:
+        print("Problemas de conexão")
+
     driver.find_element_by_xpath("//div[@class='project']").click()
-    time.sleep(2)
+    try:
+        element = WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located((By.XPATH, "//ul[@id='project-list']//li//strong[.='56A02A1']"))
+        )
+    except:
+        print("Problemas de conexão")
+
     driver.find_element_by_xpath("//ul[@id='project-list']//li//strong[.='56A02A1']").click()
-    time.sleep(7)
+
 
 def readCSV(filepath):
     with open(filepath, "r") as fh:
@@ -35,11 +59,20 @@ def readCSV(filepath):
             sites.append(site)
     return sites
 
+
 def searchLista(filepath, driver):
     sitesCSV = readCSV(filepath)
+    try:
+        element = WebDriverWait(driver, 15).until(
+            EC.presence_of_element_located((By.ID, "aGlobalSearch"))
+        )
+    except:
+        print("Problemas de conexão")
+    time.sleep(2)
     driver.find_element_by_id("rpSearchKey").send_keys(sitesCSV)
     time.sleep(2)
     driver.find_element_by_id("aGlobalSearch").click()
+
 
 def scrollToElement(element, driver):
     actions = ActionChains(driver)
